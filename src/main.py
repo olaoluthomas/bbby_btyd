deed0bafb9b9d59c57cb46744fd1aaec64dc60ed
Simeon Thomas <thomasolaoluwa@gmail.com>
2021-06-27 16:18:46 -0400

    Committing source code
    uplaoding /src

---
+++ src/main.py
@@ -0,0 +1,16 @@
+﻿# -*- coding: utf-8 -*-
+from utils import scorer
+
+input_file = "data.csv"
+output_file = "predictions.csv"
+mbg_pkl = "mbg.pkl"
+ggf_pkl = "ggf.pkl"
+t = 12
+r = 0.00764
+
+def main():
+    scorer.run_model(input_file, mbg_pkl, ggf_pkl, t, r)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/utils/scorer.py
@@ -0,0 +1,75 @@
+﻿# -*- coding: utf-8 -*-
+import pandas as pd
+import re
+from lifetimes import ModifiedBetaGeoFitter, GammaGammaFitter
+
+output_file = 'predictions.csv'
+
+def snakify(column_name):
+    """
+    Function to convert pandas column names into snake case.
+    """
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', column_name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+
+def load_params(mbg_, ggf_):
+    """
+        Load model parameters.
+    """
+    mbg = ModifiedBetaGeoFitter()
+    ggf = GammaGammaFitter()
+    mbg.load_model(mbg_)
+    ggf.load_model(ggf_)
+    return mbg, ggf
+
+
+def load_data(file):
+    """
+    Load dataset as a pandas dataframe from csv.
+    """
+    data = pd.read_csv(file)
+    data.columns = [
+        snakify(col) if col != 'T_CAL' else 'T_cal' for col in data.columns
+    ]
+    key = 'customer_id'
+    data[key] = data[key].astype("object")
+    return data
+
+
+def alive(data, mbg):
+    """
+    Predict likelihood of customers to be active purchasers
+    """
+    return mbg.conditional_probability_alive(data['frequency_cal'],
+                                             data['recency_cal'],
+                                             data['T_cal'])
+
+
+def ltv_predict(data, mbg, ggf, discount_rate, time):
+    """
+    Predict dollar value of customers from today up to time t.
+    """
+    return ggf.customer_lifetime_value(mbg,
+                                       data['frequency_cal'],
+                                       data['recency_cal'],
+                                       data['T_cal'],
+                                       data['monetary_value'],
+                                       time=time,
+                                       discount_rate=discount_rate)
+
+
+def run_model(input_file, mbg_pkl, ggf_pkl, t, r):
+    """
+    Initialize model, run predictions, and save output to file.
+
+    Args:
+
+    """
+    mbg, ggf = load_params(mbg_pkl, ggf_pkl)
+    data = load_data(file=input_file)
+    data['alive'] = alive(data, mbg)
+    data['prediction'] = ltv_predict(data, mbg, ggf, time=t, discount_rate=r)
+    data[['customer_id', 'alive', 'prediction']].to_csv(output_file,
+                                                        index=False,
+                                                        encoding='utf-8')
