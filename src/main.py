f18c4e00ff10440f9af0556c96a3f57da5ff030a
Simeon Thomas <thomasolaoluwa@gmail.com>
2021-07-16 18:17:55 -0400

    first update for training
    Instantiating the incorp_train branch to incorporate model training in addition to population scoring.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
 ﻿# -*- coding: utf-8 -*-
+from flask import Flask
 from utils import scorer
 
 input_file = "data.csv"
-output_file = "predictions.csv"
 mbg_pkl = "mbg.pkl"
 ggf_pkl = "ggf.pkl"
 t = 12
 r = 0.00764
 
+app = Flask(__name__)
+@app.route("/")
 def main():
     scorer.run_model(input_file, mbg_pkl, ggf_pkl, t, r)
 
-
 if __name__ == '__main__':
-    main()
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
